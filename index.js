087f85d0eef83a914385e86cb42db06622426aed
Trinity Cheng <j25889899gmail.com@zhengyujiede-MacBook-Pro.local>
2026-01-09 16:47:25 +0800

    fix: explicit path for images

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ mongoose
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 // API
